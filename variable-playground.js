b19375b06164b9a2623bdeff6b1b147c04d385c4
Andy Brown <rileybrown81@gmail.com>
2017-01-05 23:02:29 -0600

    Added variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 21
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array example
+
+var grades = [15, 37];
+
+function addGrades(gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	// gradesArr = [33, 33];
+	
+}
+
+addGrades(grades);
+console.log(grades);
