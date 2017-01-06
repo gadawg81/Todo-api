0b36d37db6bd0783cba18a163e6f5733b055afc6
Andy Brown <rileybrown81@gmail.com>
2017-01-05 23:01:45 -0600

    Added completed filter

--- server.js
+++ server.js
@@ -13,9 +13,21 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	console.log(queryParams);
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
+
 
 app.get('/todos/:id', function (req, res) {
 	// res.send('Asking for todo with id of ' + req.params.id);
